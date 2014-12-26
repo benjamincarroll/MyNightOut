4a4e966d3f4cde295ded0754e0ee41c2c99f0b35
Ben Carroll <benjamin.carroll@gobridgit.com>
2014-12-26 01:39:31 -0500

    Added getters and setters to DbLocation

--- app/src/main/java/com/mynightout/bencarroll/mynightout/backend/tables/DbLocation.java
+++ app/src/main/java/com/mynightout/bencarroll/mynightout/backend/tables/DbLocation.java
@@ -1,7 +1,56 @@
 package com.mynightout.bencarroll.mynightout.backend.tables;
 
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.sql.SQLException;
+
 /**
  * Created by bencarroll on 2014-12-26.
  */
+
+@DatabaseTable(tableName = "location")
 public class DbLocation extends MyNightOutObject {
+
+    public static final String LOCATION_NAME_FIELD_NAME = "location_name";
+
+    @DatabaseField(canBeNull = false, columnName = LOCATION_NAME_FIELD_NAME)
+    private String name;
+
+    @DatabaseField
+    private long createdAt;
+
+    @DatabaseField
+    private long updatedAt;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public static Dao<DbLocation, Integer> getDao(OrmLiteSqliteOpenHelper helper) throws SQLException {
+        return helper.getDao(DbLocation.class);
+    }
 }
